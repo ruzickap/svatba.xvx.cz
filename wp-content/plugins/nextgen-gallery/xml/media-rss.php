5c6a2de3f0878a439f7b93e48c48536177911178
Petr Ruzicka <petr.ruzicka@gmail.com>
2013-06-22 18:28:13 +0200

    Added httrack pages

---
+++ wp-content/plugins/nextgen-gallery/xml/media-rss.php
@@ -0,0 +1,47 @@
+<?xml version='1.0' encoding='UTF-8' standalone='yes'?>
+<rss version='2.0' xmlns:media='http://search.yahoo.com/mrss/' xmlns:atom='http://www.w3.org/2005/Atom'>
+	<channel>
+		<generator><![CDATA[NextGEN Gallery [http://nextgen-gallery.com]]]></generator>
+		<title>Svatba: Andrea a Petr</title>
+		<description></description>
+		<link><![CDATA[http://svatba.xvx.cz]]></link>
+		<atom:link rel='self' href='http://svatba.xvx.cz/wp-content/plugins/nextgen-gallery/xml/media-rss.php' type='application/rss+xml' />
+		<atom:link rel='next' href='http://svatba.xvx.cz/wp-content/plugins/nextgen-gallery/xml/media-rss.php?show=10&amp;page=1&amp;mode=last_pictures' />
+		<item>
+			<title><![CDATA[p1000313]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://svatba.xvx.cz/wp-content/gallery/petr_v_obleku/p1000313.jpg]]></link>
+			<guid>image-id:3</guid>
+			<media:content url='http://svatba.xvx.cz/wp-content/gallery/petr_v_obleku/p1000313.jpg' medium='image' />
+			<media:title><![CDATA[p1000313]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://svatba.xvx.cz/wp-content/gallery/petr_v_obleku/thumbs/thumbs_p1000313.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Svatba: Andrea a Petr (http://svatba.xvx.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[p1000312]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://svatba.xvx.cz/wp-content/gallery/petr_v_obleku/p1000312.jpg]]></link>
+			<guid>image-id:2</guid>
+			<media:content url='http://svatba.xvx.cz/wp-content/gallery/petr_v_obleku/p1000312.jpg' medium='image' />
+			<media:title><![CDATA[p1000312]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://svatba.xvx.cz/wp-content/gallery/petr_v_obleku/thumbs/thumbs_p1000312.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Svatba: Andrea a Petr (http://svatba.xvx.cz)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[p1000311]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://svatba.xvx.cz/wp-content/gallery/petr_v_obleku/p1000311.jpg]]></link>
+			<guid>image-id:1</guid>
+			<media:content url='http://svatba.xvx.cz/wp-content/gallery/petr_v_obleku/p1000311.jpg' medium='image' />
+			<media:title><![CDATA[p1000311]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://svatba.xvx.cz/wp-content/gallery/petr_v_obleku/thumbs/thumbs_p1000311.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Svatba: Andrea a Petr (http://svatba.xvx.cz)]]></media:copyright>
+		</item>
+	</channel>
+</rss>
